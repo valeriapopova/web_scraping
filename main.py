af1dd5f46c8ff7b399704ba1575a8d88e6dc1925
valeriapopova <iamvaleriapopova@yandex.ru>
2021-11-25 18:42:17 +0400

    second

--- main.py
+++ main.py
@@ -2,36 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
-response = requests.get('https://habr.com/')
-# print(response.status_code)
+URL = 'https://habr.com'
+response = requests.get(URL)
+
 
 KEYWORDS = {'дизайн', 'фото', 'web', 'Python', 'C++'}
 soup = BeautifulSoup(response.text, features='html.parser')
 
 articles = soup.find_all('article')
-# print(articles)
+
 for article in articles:
     hubs = article.find_all('span', class_="tm-article-snippet__hubs-item")
     hubs = set([hub.find('span').text for hub in hubs])
-    if KEYWORDS & hubs:
-        print('---')
-        header = article.find('h2').text
-        # print(header)
-        link = article.find('a', class_="tm-article-snippet__title-link")
-        # print(link.get('href'))
-        published = article.find('span', class_="tm-article-snippet__datetime-published")
-        published = (pub.get('time').text for pub in published)
-        # print(published)
-        print(f'Дата:{published} - Заголовок:{header} - Ссылка:{link.get("href")}')
+    link = article.find('a', class_="tm-article-snippet__title-link")
+    new_link = f"{URL}{link.get('href')}"
+    res = requests.get(new_link)
+    s = BeautifulSoup(res.text, features='html.parser')
+    text = s.find_all('div', class_="tm-article-presenter__body")
 
-
-
-
-
-
-
-
-
-
-
-
+    for a in text:
+        if KEYWORDS & hubs:
+            published = a.find('span', class_="tm-article-snippet__datetime-published")
+            published = published.find('time')['title']
+            header = a.find('h1', class_="tm-article-snippet__title tm-article-snippet__title_h1")
+            header = header.find('span').text
+            print(f'Дата:{published} - Заголовок:{header} - Ссылка:{new_link}')
